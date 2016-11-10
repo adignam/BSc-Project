e3029906a066073a99eb86fe64b1c97ac57f0ebf
adignam <aishlingxd@gmail.com>
2016-11-10 08:45:33 +0000

    Update using_reshape.py

--- using_reshape.py
+++ using_reshape.py
@@ -1,35 +1,39 @@
 #Project
 import numpy as np
+import pylab as plt
 
-size=50*50                   #Initialise size 1048576
-radius=(size/2)
+size=50*50                   #Initialise size
+radius=(size/2)              #Radius of star 
 i=0
+j=0
 #Create list
 x=np.zeros((size))
 print (x)
-
 #Reshape to form array
 x = np.reshape(x, (50,50))
 print (x)
-
 #Copy array
 y=np.rot90(x)
-
-for i in range (0,size):
-    if (x[i] < radius):
+#Populate arrays
+for i in range (0,49):
+    if (i < radius):
         x[i]=1
     i+=1
 
-print (x)
+for j in range (0,49):
+    if (j < radius):
+        x[j]=1
+    j+=1
     
-
-'''
+print (x)
+print (y)
 #Square array
-x*x
-y*y
-
+x=np.square(x*x)
+print (x)
+y=np.square(y*y)
 #Add arrays
-x+y
+r=x+y
+print (r)
 #Square root arrays
-sqrt(x+y)
-'''
+r=np.sqrt(x+y)
+plt.imshow (r)
