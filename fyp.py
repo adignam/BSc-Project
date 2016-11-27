ae72dfae7ea00de36d1779ae30ca720b499a0ee9
adignam <aishlingxd@gmail.com>
2016-11-27 18:46:00 +0000

    Update fyp.py
    Addition of fixed stellar radius

--- fyp.py
+++ fyp.py
@@ -3,17 +3,16 @@ import numpy as np
 import pylab as plt
 import math
 
-def make_star(x_axis,y_axis):
+def make_star(x_axis,y_axis,x_centre,y_centre):
+    #Makes a stellar model using the
+    #axis lengths and centre points
     size=x_axis*y_axis           #Initialise size
-    #add buffer  
-    x_centre=x_axis/2
-    y_centre=y_axis/2
-    #Create list
+    R=(x_axis/2)-20              #Radius of star with buffer of 20
+    #Create lists for x- and y- axes
     x=np.arange(size*1.) % x_axis
     y=np.arange(size*1.) % y_axis    
-    #Reshape to form array
+    #Reshape to form arrays
     x = np.reshape(x, (x_axis,y_axis))
-    #y array
     y=np.rot90(np.reshape(y, (x_axis,y_axis)))
     #Subtract centre points
     x=x-x_centre
@@ -22,14 +21,23 @@ def make_star(x_axis,y_axis):
     x=np.square(x)
     y=np.square(y)    
     r1=np.add(x,y)
-    #Square root arrays
+    #Square root arrays to obtain circles
     r=np.sqrt(r1)
-    plt.imshow (r)
-    
+
+    #Make circle of stellar radius
+    for i in range (0,(x_axis-1)):
+        for j in range(0,(y_axis-1)):
+            #Outside the stellar radius will be zeroed     
+            if (r[i][j]>R):
+                r[i][j]=0.0
+    #Plot stellar model    
+    plt.imshow(r, cmap=plt.cm.binary)
+
 def limb_darkening(a,b,gamma):
+    #Calculates quadratic limb darkening coefficient
     mu=math.cos(gamma)
     LD=1-(a*(1-mu))-(b*((1-mu)**2))
     print (LD)
     
-make_star(1024.,1024.)
-limb_darkening(0.3,0.1,57)
+make_star(1024,1024,512,512)
+limb_darkening(0.3,0.1,0)
