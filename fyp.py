8dc4f764a25def11c3029110a561547a5ecd19d6
adignam <aishlingxd@gmail.com>
2016-12-07 10:36:27 +0000

    Update fyp.py
    Adding in the Gaussian function to get the spectrum. Need to add the rotational velocity and the planet transit

--- fyp.py
+++ fyp.py
@@ -3,16 +3,18 @@ import numpy as np
 import pylab as plt
 import math
 
-def make_star(x_axis,y_axis,x_centre,y_centre):
-    #Makes a stellar model taking the
-    #axis lengths and centre points as parameters
+def make_star(x_axis,y_axis):
     size=x_axis*y_axis           #Initialise size
     R=(x_axis/2)-20              #Radius of star with buffer of 20
-    #Create lists for x- and y- axes
+    x_centre=x_axis/2
+    y_centre=y_axis/2
+    
+    #Create list
     x=np.arange(size*1.) % x_axis
     y=np.arange(size*1.) % y_axis    
-    #Reshape to form arrays
+    #Reshape to form array
     x = np.reshape(x, (x_axis,y_axis))
+    #y array
     y=np.rot90(np.reshape(y, (x_axis,y_axis)))
     #Subtract centre points
     x=x-x_centre
@@ -21,19 +23,36 @@ def make_star(x_axis,y_axis,x_centre,y_centre):
     x=np.square(x)
     y=np.square(y)    
     r1=np.add(x,y)
-    #Square root arrays to obtain circles
+    #Square root arrays
     r=np.sqrt(r1)
     #Make circle of given stellar radius
     mask=(r<=R)
     r=r*mask
     #Plot stellar model    
-    plt.imshow(r, cmap=plt.cm.binary) 
-
+    plt.imshow(r, cmap=plt.cm.binary)
+    return r
+    
 def limb_darkening(a,b,gamma):
-    #Calculates quadratic limb darkening coefficient
     mu=math.cos(gamma)
     LD=1-(a*(1-mu))-(b*((1-mu)**2))
     print (LD)
     
-make_star(1024,1024,512,512)
+def spectrum(r, A, sigma):
+    y_flux=np.cumsum(r)
+    x_flux=np.cumsum(y_flux)
+    print(x_flux)
+    v=-10
+    vx=10
+    Gauss=1-A*np.exp((-(v-vx)**2)/(2*sigma**2))
+    print("Gauss=",Gauss)
+    Fv=x_flux*Gauss
+    print(Fv)
+    plt.plot(Fv)
+    plt.ylim([0,1*10**17])
+    plt.xlim([0,1*10**17])
+
+
+    
+r=make_star(1024,1024)
 limb_darkening(0.3,0.1,0)
+spectrum(r, 0.9, 3)
