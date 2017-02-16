f8241663bd805baba398b975d950f279caebc98d
adignam <aishlingxd@gmail.com>
2017-02-16 13:38:25 +0000

    Update fyp.py

--- fyp.py
+++ fyp.py
@@ -3,9 +3,9 @@ import numpy as np
 import pylab as plt
 import math
 
-def make_star(x_axis,y_axis):
+def make_star(x_axis,y_axis,R):
     size=x_axis*y_axis           #Initialise size
-    R=(x_axis/2)-20              #Radius of star with buffer of 20
+    R=(R)-20                     #Radius of star with buffer of 20
     x_centre=x_axis/2
     y_centre=y_axis/2
     
@@ -41,25 +41,32 @@ def limb_darkening(a,b,gamma):
 def spectrum(r, A, sigma):  
     #Equatorial Velocity
     veq=2.
-    vrot=(np.arange(1025)-512)/512.*veq
-    v=np.arange(-12,12,0.02)  #-12....12
-    flux=np.zeros(1025)
+    vrot=(np.arange(1024)-512)/512.*veq
+    v=np.arange(-12,12,0.02345)  #-12....12
+    flux=np.zeros(np.shape(v))
 
     #Gaussian
-    fstar=np.sum(r,0)
+    fstar=-np.sum(r,0) 
     i=0
-    for vx in vrot :
+    for vx in vrot:
         #Gaussian calculation
         Gauss=1-A*np.exp((-(v-vx)**2)/(2*sigma**2))
-        print("Gauss=",Gauss)
         #Calculate flux
-        flux[i,:]=fstar[i]*Gauss
+        flux=fstar[i]*Gauss
         i+=1
-    plt.plot(v,flux.sum(1))
+    #plt.plot(v,flux)    
+
+def planet_motion(planet,coeff,i,phase):
+    x=coeff*np.sin(2*np.pi*phase)
+    y=coeff*np.cos(i)*np.cos(2*np.pi*phase)
+    plt.plot(planet)
+    #plt.plot(planet[200,500])
     
-    
-    
-r=make_star(1024,1024)
-r=r-limb_darkening(0.3,0.1,1)
-spectrum(r, 0.9, 2)
-#plt.imshow(r, cmap=plt.cm.binary)
+star=make_star(1024,1024,512)
+star=star-limb_darkening(0.3,0.1,1)
+spectrum(star, 0.9, 2)
+planet=make_star(1024,1024,256)
+system=star+planet
+spectrum(system, 0.9, 2)
+planet_motion(planet,8.84,1.14959,1)
+#plt.imshow(system, cmap=plt.cm.binary)
