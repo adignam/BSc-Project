b7093f2184f95cf62f5b083cda5ed12f7d853159
adignam <aishlingxd@gmail.com>
2017-03-16 15:00:14 +0000

    Update fyp.py

--- fyp.py
+++ fyp.py
@@ -38,43 +38,52 @@ def limb_darkening(a,b,r,mask):
     LD=r*0.
     LD[mask==1]=1-(a*(1-mu[mask==1]))-(b*((1-mu[mask==1])**2))
     return LD
-    
-def spectrum(r, A, sigma):  
-    #Plots the spectrum of the star
-    veq=2.
+
+def gauss(a,sigma,veq):
     vrot=(np.arange(1024)-512)/512.*veq
-    v=np.arange(-12,12,0.02345)  
+    v=np.arange(-12,12,0.02345)     
+    for vx in vrot:
+        gauss=1-a*np.exp((-(v-vx)**2)/(2*sigma**2))
+    return gauss,v
+    
+def spectrum(r,v,gauss):  
+    #Plots the spectrum of the star
     flux=np.zeros(np.shape(v))
     fstar=-np.sum(r,0) 
-    vx=np.zeros(len(vrot))
-    
-    Gauss=1-A*np.exp((-(v-vx)**2)/(2*sigma**2))
-    for i in range(0,len(vrot)):       
-        flux=fstar[i]*Gauss
+    for i in range(0,len(v)):       
+        flux=fstar[i]*gauss
         i+=1
-    plt.plot(v,-flux)    
-
+    return flux    
     
 def planet_motion(coeff,i,Rstar,phase,phase_bin,centre):
-    
     phase=np.arange(-phase,phase,phase_bin)
     x=coeff*np.sin(2*np.pi*phase)*Rstar+centre   
     y=coeff*np.cos(i)*np.cos(2*np.pi*phase)*Rstar+centre
     Rplanet = Rstar/2
+    
+    star,mask1=make_circle(1024,1024,510)
+    #print(mask1[512,512])                   #true
+    #plt.imshow(mask1, cmap=plt.cm.binary) #black disk obtained
+    star=-limb_darkening(0.3,0.3,star,mask1)
+    plt.imshow(star)   
+    plt.imshow(star, cmap=plt.cm.binary) 
+    star=np.nan_to_num(star)
+    print(star[400,200])
+    plt.imshow(star, cmap=plt.cm.binary)   
 
+    g,v=gauss(0.9,0.7,2.)
+    '''
     for i in range(0,200):
         r,mask=make_circle(1024,1024,Rplanet,x[i],y[i])
         mask=1-mask
-        spectrum(mask, 0.9, 0.7)
-        star=limb_darkening(0.3,0.3,r,mask)*mask
-        spectrum(star, 0.9, 0.7)
+        plt.imshow(mask)
+        #plt.imshow(r)
+        #spectrum(star,v,g)
+        '''
         
-        
-star=make_circle(1024,1024,510)           #make star 
-
-planet=make_circle(1024,1024,256)         #make planet
-
+#star=make_circle(1024,1024,510)           #make star 
+#planet,m=make_circle(1024,1024,256)         #make planet
 planet_motion(8.84,1.,512,0.1,0.001,512)            # coeff=8.84,i = 1.14959 for hd189733b
 
-#plt.imshow(star, cmap=plt.cm.binary)
+#plt.imshow(planet, cmap=plt.cm.binary)
 #scipy.misc.imsave('project_image.jpg', -star)
