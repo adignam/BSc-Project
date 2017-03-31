2d19ae483d16d436bad5faca1af907af5ad82302
adignam <aishlingxd@gmail.com>
2017-03-31 10:34:31 +0100

    Update fyp.py

--- fyp.py
+++ fyp.py
@@ -42,10 +42,14 @@ def limb_darkening(a,b,r,mask):
     LD[mask==1]=1-(a*(1-mu[mask==1]))-(b*((1-mu[mask==1])**2))
     return LD
 
-def gauss(a,sigma,veq,v):
-    vrot=(np.arange(1024)-512)/512.*veq
-    #v=np.arange(-12,12,0.02345)    
-    gauss=np.zeros( (vrot.shape[0],v.shape[0]) )
+def gauss(a,sigma,v):
+    vrot=(np.arange(1024)-512)/512.*2.5
+    #v=np.arange(-12,12,0.02345)   
+    x=np.shape(vrot)
+    y=np.shape(v)
+    x=np.rint(x)
+    y=np.rint(y)
+    gauss=np.zeros((1024,161))
     i=0
     for vx in vrot:
         gauss[i,:]=1-a*np.exp((-(v-vx)**2)/(2*sigma**2))
@@ -57,24 +61,25 @@ def spectrum(array,g,v):
     
     flux=np.zeros(np.shape(v))
     fstar=np.sum(array,0) 
-    for i in range(0,len(v)):       
+    for i in range(0,161):       
         flux=flux+fstar[i]*g[i,:]
         i+=1
     flux=flux/np.median(flux[((v<-12)|(v>12))])        
     return flux   
     
     
-def planet_motion(aRstar,incl,Rstar,phase,phase_bin,centre,v):
+def planet_motion(aRstar,incl,Rstar,phase,phase_bin,centre,v,amp,line_width):
     #phase=np.arange(-phase,phase,phase_bin)
     x=aRstar*np.sin(2*np.pi*phase)*Rstar+centre   
     y=aRstar*np.cos(incl)*np.cos(2*np.pi*phase)*Rstar+centre
     Rplanet = Rstar/10.
-    g=gauss(0.9,2.7,2.,v)
+    g=gauss(amp,line_width,v)
     
     star,mask1=make_circle(1025,1025,Rstar)
     star=limb_darkening(0.3,0.3,star/Rstar,mask1)
-    star=np.nan_to_num(star)    
-    flux=np.zeros( (len(phase),len(v)) )
+    star=np.nan_to_num(star)  
+    print(np.shape(v))
+    flux=np.zeros( (111,161) )
     for i in range(0 ,len(phase)):
         planet,mask=make_circle(1025,1025,Rplanet,x[i],y[i])
         system=star*(1.-mask)
@@ -125,24 +130,25 @@ def get_phase():
     return FinalP, data, v
 
 def mcmc(parameters,stepsize,phase,data,v):
-    
-    aRstar=1.           #8.84
-    incl=90.*np.pi/180.                #85.71
-    Rstar=500
-    centre=512
+    constant=[0,1,2,3,4,5]
+    constant[0]=1.           #8.84aRstar
+    constant[1]=85.71*np.pi/180.   #incl
+    constant[2]=500               #Rstar
+    constant[3]=512              #centre
+    constant[4]=0.816                   #u1
+    constant[5]=0                       #u2
 
-    nstep=20
+    nstep=200
     npars=np.size(parameters)                          
     error=0.00015
     u1=0.3
     u2=0.3
     #phase=0.1
-
     chain=np.zeros((npars,nstep))
     chi_squared=np.zeros(nstep)
     chain[:,0]=parameters
 
-    model=planet_motion(aRstar,incl,Rstar,phase,0.001,centre,v)
+    model=planet_motion(constant[0],constant[1],constant[2],phase,0.001,constant[3],v,parameters[2],parameters[3])
     flux=data
 
     chisq=np.sum((flux-model)**2/error**2 )
@@ -151,7 +157,7 @@ def mcmc(parameters,stepsize,phase,data,v):
     for i in range(1,nstep):
         ##Make a random jump in parameter space
         new_parameters=chain[:,i-1]+np.random.normal(0.,1.,npars)*stepsize
-        model=planet_motion(new)
+        model=planet_motion(constant[0],constant[1],constant[2],phase,0.001,constant[3],new_parameters[4],new_parameters[2],new_parameters[3])
         nchisq=np.sum((flux-model)**2/error**2 )
         dchi=nchisq-chisq
         ## Going to lower chi squared: Accept new value
@@ -184,5 +190,4 @@ e_vsini=0.2
 stepsize=[e_Rp_Rstar,e_lamda,e_amplitude,e_line_width,e_vsini]
 phase,data,v=get_phase()
 parameters=[Rp_Rstar,lamda,amplitude,line_width,vsini]
-stepsize=[]
 mcmc(parameters,stepsize,phase,data,v)
